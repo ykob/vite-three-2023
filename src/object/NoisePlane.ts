18fdd06f01e43882016a0e54b0f06e2f3d1093b3
ykob <info@tplh.net>
2023-04-04 00:21:52 +0900

    added shaders for Noise Plane.

--- src/object/NoisePlane.ts
+++ src/object/NoisePlane.ts
@@ -1,9 +1,14 @@
 import * as THREE from "three";
+import vs from "./glsl/noisePlane.vs";
+import fs from "./glsl/noisePlane.fs";
 
 export class NoisePlane extends THREE.Mesh {
   constructor() {
     const geometry = new THREE.PlaneGeometry(2, 2, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+    const material = new THREE.RawShaderMaterial({
+      vertexShader: vs,
+      fragmentShader: fs,
+    });
     super(geometry, material);
   }
   resize(width: number) {
