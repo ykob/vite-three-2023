da703de3f7b1ce3358dc05b52d70e44af4bd75ec
ykob <info@tplh.net>
2023-04-03 21:10:42 +0900

    added the way to render the 3d scene.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+
+const canvas = document.getElementById("canvas-webgl") as HTMLCanvasElement;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+
+const resize = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  renderer.setSize(width, height, false);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+};
+
+const update = () => {
+  renderer.render(scene, camera);
+  requestAnimationFrame(update);
+};
+
+const init = () => {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+  const cube = new THREE.Mesh(geometry, material);
+
+  scene.add(cube);
+  camera.position.z = 2;
+
+  resize();
+  update();
+};
+
+init();
+
+window.addEventListener("resize", () => {
+  resize();
+});
