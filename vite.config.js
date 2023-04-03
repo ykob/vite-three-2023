c1f4ffd1828c37baa6f41e767cf8785601b8ae35
ykob <info@tplh.net>
2023-04-04 00:50:39 +0900

    yarn remove vite-plugin-glsl

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
-import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    glsl({
-      include: ["**/*.glsl", "**/*.vs", "**/*.fs"],
-      warnDuplicatedImports: true,
-      watch: true,
-    }),
-  ],
+  plugins: [],
 });
