a3c3a32f281a156647370f982b64726fcfb60c05
ykob <info@tplh.net>
2023-04-04 23:27:17 +0900

    added uniforms on NoisePlane mesh.

--- src/object/NoisePlane.ts
+++ src/object/NoisePlane.ts
@@ -2,16 +2,25 @@ import * as THREE from "three";
 import vs from "./glsl/NoisePlane.vs?raw";
 import fs from "./glsl/NoisePlane.fs?raw";
 
-export class NoisePlane extends THREE.Mesh {
+export class NoisePlane extends THREE.Mesh<
+  THREE.BufferGeometry,
+  THREE.RawShaderMaterial
+> {
   constructor() {
     const geometry = new THREE.PlaneGeometry(2, 2, 1, 1);
     const material = new THREE.RawShaderMaterial({
       vertexShader: vs,
       fragmentShader: fs,
+      uniforms: {
+        time: { value: 0 },
+      },
     });
     super(geometry, material);
   }
   resize(width: number) {
     this.scale.set(width, 1, 1);
+  }
+  update(time: number) {
+    this.material.uniforms.time.value += time;
   }
 }
