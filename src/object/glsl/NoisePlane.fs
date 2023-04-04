a632afcc666035f26375fa949b93835e853d958a
ykob <info@tplh.net>
2023-04-04 23:27:02 +0900

    added white noise on fragment shader.

--- src/object/glsl/NoisePlane.fs
+++ src/object/glsl/NoisePlane.fs
@@ -3,6 +3,11 @@ precision highp float;
 varying vec3 vPosition;
 varying vec2 vUv;
 
+float calcWhiteNoise(vec2 uv, float scale) {
+  return fract(sin(dot(uv, vec2(12.9898, 78.233))) * 43758.5453 * scale);
+}
+
 void main() {
-  gl_FragColor = vec4(1.0 - vUv, 1.0, 1.0);
+  float noise = calcWhiteNoise(vUv, 1.0);
+  gl_FragColor = vec4(vec3(noise), 1.0);
 }
