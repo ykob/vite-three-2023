9a7e312eecd97473260b9bcf5cfb9a4e0d57801f
ykob <info@tplh.net>
2023-04-06 19:52:39 +0900

    render the dom elements by JavaScript.

--- src/main.ts
+++ src/main.ts
@@ -48,3 +48,17 @@ init();
 window.addEventListener("resize", () => {
   resize();
 });
+
+const contentDom = document.getElementById("content");
+
+if (contentDom) contentDom.innerHTML = `
+<img
+  class="vite-logo"
+  src="vite.svg"
+  width="240"
+  height="240"
+  decoding="async"
+  alt="Vite"
+/>
+<div class="title">Vite + three.js</div>
+`;
