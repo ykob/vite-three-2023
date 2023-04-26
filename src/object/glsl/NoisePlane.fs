0ac763f6e35f4788611f9bbc5e14da3da49dcf08
ykob <info@tplh.net>
2023-04-26 21:41:33 +0900

    adjusted the noise effect.

--- src/object/glsl/NoisePlane.fs
+++ src/object/glsl/NoisePlane.fs
@@ -13,8 +13,8 @@ float calcWhiteNoise(vec2 uv, float scale) {
 void main() {
   vec4 color = texture2D(uTexture, vUv);
   vec2 uv = vUv + (color.rb * 2.0 - 1.0) * color.g;
-  float noise = calcWhiteNoise(floor((gl_FragCoord.xy) * 0.2) + vec2(0.0, uTime), 1.0);
-  float noiseV1 = calcWhiteNoise(floor((gl_FragCoord.xy) * 0.02 * vec2(0.0, 1.0)) + sin(uTime), 1.0);
-  float noiseV2 = calcWhiteNoise(floor((gl_FragCoord.xy) * 0.1 * vec2(0.0, 1.0)) + cos(uTime), 1.0);
-  gl_FragColor = vec4(vec3(uv.x, 0.0, uv.y) * 0.8 + vec3(noise * 0.2 + smoothstep(0.97, 1.0, noiseV1) * 0.1 + smoothstep(0.9, 1.0, noiseV2) * 0.1), 1.0);
+  float noise = calcWhiteNoise(floor((gl_FragCoord.xy * 0.18)) + vec2(0.0, uTime), 1.0);
+  float noiseV1 = calcWhiteNoise(floor((gl_FragCoord.xy * 0.02) * vec2(0.0, 1.0)) + sin(uTime), 1.0);
+  float noiseV2 = calcWhiteNoise(floor((gl_FragCoord.xy * 0.1) * vec2(0.0, 1.0)) + cos(uTime), 1.0);
+  gl_FragColor = vec4(vec3(uv.x, 0.0, uv.y) * 0.8 + vec3(noise * 0.2 + smoothstep(0.99, 1.0, noiseV1) * 0.1 + smoothstep(0.95, 1.0, noiseV2) * 0.1), 1.0);
 }
